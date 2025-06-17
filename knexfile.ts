cf810b6a91126d4414b50ee0a003decb7190e15e
guarinFE <victorhugoguarin@hotmail.com>
2025-06-17 14:58:50 -0300

    alterando o knexfile-ts

--- knexfile.ts
+++ knexfile.ts
@@ -2,6 +2,7 @@ import { Knex } from 'knex'
 import path from 'path'
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
+import 'dotenv/config'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
@@ -17,15 +18,26 @@ export const config: { [key: string]: Knex.Config } = {
       extension: 'ts',
     },
   },
+
   test: {
     client: 'sqlite3',
     connection: {
-      filename: path.resolve(__dirname, 'src', 'database', 'test.db', ), // ou use env.DATABASE_URL
+      filename: path.resolve(__dirname, 'src', 'database', 'test.db'),
     },
     useNullAsDefault: true,
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
       extension: 'ts',
     },
-  }
+  },
+
+  production: {
+    client: process.env.DATABASE_CLIENT,     // “pg”
+    connection: process.env.DATABASE_URL,    // a URL do Render
+    useNullAsDefault: true,
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+      extension: 'ts',
+    },
+  },
 }
